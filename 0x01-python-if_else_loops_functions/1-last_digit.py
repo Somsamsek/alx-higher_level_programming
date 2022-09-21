f6fb1de10b8c7c8862c24a42afbe96f7ed5e6f40
Somsamsek <170404060@live.unilag.edu.ng>
2022-09-20 20:59:27 -0700

    Third commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,12 @@ if number < 0:
 else:
     last_digit = number % 10
 
-if last_digit is 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-elif last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+if last_digit > 5:
+    print("Last digit of {} is {} and is greater than 5"
+        .format(number, last_digit))
+elif last_digit is 0:
+    print("Last digit of {} is {} and is 0"
+	.format(number, last_digit))
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print("Last digit of {} is {} and is less than 6 and not 0"
+        .format(number, last_digit))
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
     if i is not (ord('q')) and i is not (ord('e')):
-        print('{}'.format(chr(i)), end='')
+        print('{}'.format(chr(i)))
