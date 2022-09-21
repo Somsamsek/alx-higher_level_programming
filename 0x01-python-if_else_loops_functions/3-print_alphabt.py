553762b54421a57f83f3be13fa81d41381f5bbba
Somsamsek <170404060@live.unilag.edu.ng>
2022-09-20 20:43:39 -0700

    Second commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(65, 91):
-	if ((i != 81) and (i != 69):
-			print(chr(i))
+for i in range(ord('a'), ord('z')+1):
+    if i is not (ord('q')) and i is not (ord('e')):
+        print('{}'.format(chr(i)), end='')
