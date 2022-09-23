bc8a5f814bcb6f5656e9cfba415ba39f0459d1d8
Somsamsek <170404060@live.unilag.edu.ng>
2022-09-22 19:55:04 -0700

    All of 'em

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    userin = argv[1:]
+    size = len(userin)
+    print("{:d} {:s}{:s}".format(size, "arguments"
+        if (size) is not 1
+        else "argument", "."
+        if (size) is 0
+        else ":"))
+    for idx, arg in enumerate(userin):
+        print("{:d}: {:s}".format(idx + 1, arg))
