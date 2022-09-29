207cb3219d2309355f9958831720c616011b9845
Somsamsek <170404060@live.unilag.edu.ng>
2022-09-28 19:18:52 -0700

    Just 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list is not None:
-        for i in set(my_list):
-            uniq += i
-        return (uniq)
-    return None
+    for i in set(my_list):
+        uniq += i
+    return (uniq)
