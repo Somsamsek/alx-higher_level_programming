597b0f6b38b237508f2d0382072dc42dbc7dbb4c
Somsamsek <seke@live.unilag.edu.ng>
2024-01-04 15:43:25 +0000

    Calc

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,15 +4,15 @@ if __name__ == "__main__":
     i = len(argv) - 1
 
     if i == 0:
-    print("{} arguments.".format(i))
+    	print("{} arguments.".format(i))
     elif i == 1:
-    print("{} argument:".format(i))
+    	print("{} argument:".format(i))
     else:
-    print("{} arguments:".format(i))
+    	print("{} arguments:".format(i))
 
     if i >= 1:
-    i = 0
-    for arg in argv:
-    if i != 0:
-    print("{}: {}".format(i, arg))
-    i += 1
+    	i = 0
+    	for arg in argv:
+    		if i != 0:
+    			print("{}: {}".format(i, arg))
+    				i += 1
