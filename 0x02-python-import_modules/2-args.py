3534b5f82a0e2731a8744273b5ed815799648fff
Somsamsek <seke@live.unilag.edu.ng>
2024-01-04 15:45:37 +0000

    Calc2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    i = len(argv) - 1
+    import sys
+    i = len(sys.argv) - 1
 
     if i == 0:
-    	print("{} arguments.".format(i))
+        print("{} arguments.".format(i))
     elif i == 1:
-    	print("{} argument:".format(i))
+        print("{} argument:".format(i))
     else:
-    	print("{} arguments:".format(i))
+        print("{} arguments:".format(i))
 
     if i >= 1:
-    	i = 0
-    	for arg in argv:
-    		if i != 0:
-    			print("{}: {}".format(i, arg))
-    				i += 1
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
